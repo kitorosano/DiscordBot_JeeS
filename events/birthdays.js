7470bd06b8a2708696c8098080d72fdb842e8f6a
Kitorosano <rosanoesteban2@gmail.com>
2020-12-27 17:15:46 -0300

    month arr for mm

--- events/birthdays.js
+++ events/birthdays.js
@@ -10,6 +10,8 @@ mongoose.connect(mongo, {
   useFindAndModify: false
 })
 
+const months = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12};
+
 module.exports = { 
   async getEvents(today) {
     const day = `${today[2]}:${months[today[1]]}`
@@ -25,6 +27,8 @@ module.exports = {
     console.log(channel.name)
     const user = await guild.member(event.userID);
     console.log(user.username)
+
+    // DARLE AL USUARIO ROL CUMPLEAÑERO
 
     const MsgBday = new MessageEmbed()
           .setColor('YELLOW')
