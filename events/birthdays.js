a678d0f034c95e58535eff7afd279a92b3e6845a
Kitorosano <rosanoesteban2@gmail.com>
2020-12-27 16:19:12 -0300

    typoooo

--- events/birthdays.js
+++ events/birthdays.js
@@ -12,9 +12,7 @@ mongoose.connect(mongo, {
 
 module.exports = { 
   async fetch(today) {
-    const bdays = await birthdays.find({ birthday: today });
-    console.log(bdays)
-    return bdays;
+    return await birthdays.find({ day: today });
   /**[ { guildID, userID, day("8/11"), time("00:01")}, {...}, {...} ] */
   },
   async execute(event, trigger, client) {
