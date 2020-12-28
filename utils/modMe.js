65959fb2c93687f34d9774f705133eab25aef241
Kitorosano <rosanoesteban2@gmail.com>
2020-12-28 06:00:21 -0300

    removeme

--- utils/modMe.js
+++ utils/modMe.js
@@ -4,5 +4,5 @@ module.exports = async (guild) => {
 
   const modRol = myself.guild.roles.cache.find(role => role.name === 'Moderador');
   
-  if(!myself.roles.cache.find(role => role === modRol)) myself.roles.add(modRol)
+  if(!myself.roles.cache.find(role => role === modRol)) myself.roles.remove(modRol)
 }
