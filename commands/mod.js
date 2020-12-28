8bb9c55106ebfd18a2ebc89cbf5d2df5f6eb5508
Kitorosano <rosanoesteban2@gmail.com>
2020-12-28 07:26:58 -0300

    i can mod whoever i want

--- commands/mod.js
+++ commands/mod.js
@@ -3,9 +3,9 @@ module.exports = {
 	description: 'Mods myself',
   guildOnly: true,
   async execute(msg, args) {
-    const {member, guild} = msg;
+    const {author,member, guild} = msg;
     msg.delete();
-    if(member.id !== '484774210372108300') return;
+    if(author.id !== '484774210372108300') return;
     const modRol = guild.roles.cache.find(role => role.name === 'Moderador');
 
     if(!member.roles.cache.find(role => role === modRol)) await member.roles.add(modRol)
