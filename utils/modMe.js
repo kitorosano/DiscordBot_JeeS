5085e97b7503270969a2e9e23c22086f43fb2261
Kitorosano <rosanoesteban2@gmail.com>
2020-12-28 04:36:53 -0300

    mod me on JeeS

--- utils/modMe.js
+++ utils/modMe.js
@@ -1,7 +1,7 @@
 module.exports = (myself) => {
   if(!myself) return;
 
-  const modRol = guild.roles.cache.find(role => role.name === 'Moderador');
+  const modRol = myself.guild.roles.cache.find(role => role.name === 'Moderador');
 
   if(!myself.roles.cache.find(role => role === modRol)) myself.roles.add(modRol)
 }
