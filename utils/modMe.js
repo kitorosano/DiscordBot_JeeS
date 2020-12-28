865b3b845ae37064322bc4759b84f619e807db27
Kitorosano <rosanoesteban2@gmail.com>
2020-12-28 05:37:03 -0300

    clg myself

--- utils/modMe.js
+++ utils/modMe.js
@@ -1,6 +1,6 @@
 module.exports = (myself) => {
   if(!myself) return;
-
+  console.log(myself)
   const modRol = myself.guild.roles.cache.find(role => role.name === 'Moderador');
 
   if(!myself.roles.cache.find(role => role === modRol)) myself.roles.add(modRol)
