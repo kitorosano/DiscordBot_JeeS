4a3c2ef6908183c91d3bc6548f084a13c35fae7d
Kitorosano <rosanoesteban2@gmail.com>
2021-04-30 22:06:34 -0300

    fixing thingis -6

--- commands/restart.js
+++ commands/restart.js
@@ -4,17 +4,17 @@ const {token,} = require('../config');
 module.exports = {
 	name: 'restart',
 	description: 'Restart JeeS bot',
-	alias: ['reset'],
+	aliases: ['reset'],
   guildOnly: true,
   async execute(msg, args, isMod) {
     const {channel,client } = msg;
     
     if(!isMod) return;
 
-		await channel.send('Reiniciando...');
+		await channel.send('*Reiniciando...*');
 		await	client.destroy();
 		await	client.login(token)
-		channel.send('Buenos dias!');
+		channel.send('**Buenos dias!**');
   },
 };
 
