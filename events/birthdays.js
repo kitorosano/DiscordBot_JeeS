053fd0b97a322431e88093736cae79c4b469bf3b
Kitorosano <rosanoesteban2@gmail.com>
2021-05-01 04:10:32 -0300

    fixing bday -18

--- events/birthdays.js
+++ events/birthdays.js
@@ -30,6 +30,7 @@ module.exports = {
 
     // const BdayRole = guild.roles.cache.find(role => role.name === 'Cumpleañer@');
     const roles = await guild.roles.fetch();
+    console.log(roles)
     const BdayRole = roles.filter(role => role.name === 'Cumpleañer@');
     member.roles.add(BdayRole)
 
