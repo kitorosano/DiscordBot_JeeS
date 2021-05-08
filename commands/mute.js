a7b1b6662b5c3a46abfd766e2394727b0c087d98
Kitorosano <rosanoesteban2@gmail.com>
2021-05-07 22:08:21 -0300

    fixing mute/unmute

--- commands/mute.js
+++ commands/mute.js
@@ -7,15 +7,16 @@ module.exports = {
   guildOnly: true,
   modOnly: true,
   args: true,
-  execute(msg, args, isMod) {
+  async execute(msg, args, isMod) {
     const {guild, mentions, channel} = msg;
     const [who, ...why] = args;
     const razon = why.length > 0 ? why.join(' ') : 'Sin especificar';
     const target = mentions.users.first();
     if(!target) return channel.send(new MessageEmbed().setColor("RED").setAuthor(`Miembro no encontrado`))
 
-    const mutedRole = guild.roles.cache.find(role => role.name === 'Silenciado');
-    const memberTarget = guild.members.cache.get(target.id)
+    const roles = await guild.roles.fetch();
+    const mutedRole = roles.cache.find(role => role.name === 'Silenciado');
+    const memberTarget = await guild.members.fetch(target.id);
 
     if(memberTarget.roles.cache.find(role => role !== mutedRole)) {
       memberTarget.roles.add(mutedRole); //AGREGA EL ROL
