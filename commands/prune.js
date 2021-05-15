085cdd83c0e42d4b5ebd5b7aa8846ede9d89a8f1
Kitorosano <rosanoesteban2@gmail.com>
2021-05-15 03:27:08 -0300

    testing new command prune -17

--- commands/prune.js
+++ commands/prune.js
@@ -42,7 +42,8 @@ module.exports = {
 					channel.send(new MessageEmbed().setColor("GREEN").setAuthor(`Se han eliminado ${mensajes.size * count}/${many} mensajes en total de los usuarios mencionados.`))
 					.then(msg => {count++; setTimeout(() => msg.delete() ,5000)})
 				})
-				.catch(() => channel.send(new MessageEmbed().setColor("RED").setAuthor(`Algo malio sal...`).setFooter('Recuerda que no puedo eliminar mensajes anteriores a 2 semanas')))
+				.catch(() => console.error)
+				// channel.send(new MessageEmbed().setColor("RED").setAuthor(`Algo malio sal...`).setFooter('Recuerda que no puedo eliminar mensajes anteriores a 2 semanas'))
 			});
 		}
 	},
