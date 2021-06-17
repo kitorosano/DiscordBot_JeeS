03864434371734c11b298c20ff29ace2c25e360a
Kitorosano <rosanoesteban2@gmail.com>
2021-06-17 20:00:34 -0300

    testing anime - 1

--- commands/anime.js
+++ commands/anime.js
@@ -11,7 +11,7 @@ module.exports = {
     const {author, guild, mentions, channel} = msg;
 
 		if (!args.length) { //si no hay argumentos
-			const schedule = _anime.schedule()
+			const schedule = await _anime.schedule()
 			console.log(schedule)
 		}
 
