537dab4ba5f67fa065a13c03799f6d6f22b62bab
Kitorosano <rosanoesteban2@gmail.com>
2021-06-17 22:59:39 -0300

    fixing anime - 7

--- commands/anime.js
+++ commands/anime.js
@@ -10,16 +10,14 @@ module.exports = {
   async execute(msg, args, isMod) {
     const {author, guild, mentions, channel} = msg;
 
-		const MsgToSend = new MessageEmbed().setColor('#ffff55');
+		const MsgToSend = new MessageEmbed()
+											.setColor('#ffff55')
+											.setFooter(':globe_with_meridians: Provisto por animeschedule.net', client.user.displayAvatarURL())
 
 		if (!args.length) {
-			MsgToSend.setTitle(`Animes en Emision del dia:`);
-			
 			const schedule = await _anime.schedule(new Date().getDay())
-			schedule.map(anime => {
-				const {title, episode, time, poster} = anime;
-				MsgToSend.addField('', `**${time}** | ${title} *Ep${episode}*`)
-			})
+			MsgToSend.setTitle(`:alarm_clock: Animes en Emision del dia:`)
+							 .addField("Visitar animeflv", schedule.map(anime => `**${anime.time}** | ${anime.title} *Ep${anime.episode}*`))
 		};
 
 		return channel.send(MsgToSend)
