5aa45be6ab92525cc473f55ba723e75e8951d5cf
Kitorosano <rosanoesteban2@gmail.com>
2022-03-06 17:41:16 -0300

    Added new features to ¡anime

--- commands/anime.js
+++ commands/anime.js
@@ -13,16 +13,37 @@ const dias = [
 
 module.exports = {
 	name: 'anime',
-	description: 'Va de anime',
+	description: 'Calendario de capitulos de anime de la semana.',
 	// aliases: [''],
-	// usage: '[usuario]',
+	usage: '[date]',
 	guildOnly: true,
 	async execute(msg, args, isMod) {
 		const { author, guild, channel, client } = msg;
 
-		const day = new Date().getHours() < 3 ? new Date().getDay() - 1 : new Date().getDay();
+    let date, field_date, field_day, field_month;
+    if(!args.length) { // today
+      date = new Date().getHours() < 3 ? new Date().getDay() - 1 : new Date().getDay();
+      field_date = dias[date];
+      field_day = new Date().getDate();
+      field_month = new Date().getMonth() +1;
+    } else { //any other day
+      date = dias.indexOf(args[0].toUpperCase());
+      field_date = dias[date];
+      let _otherDay = new Date();
+      while(_otherDay.getDay() !== date)
+        _otherDay.setDate(_otherDay.getDate() - 1);
+      field_day = _otherDay.getDate();
+      field_month = _otherDay.getMonth() +1;
+    }
 
-		const schedule = await _anime.schedule(day);
+		const schedule = await _anime.schedule(date, true);
+		const animeSchedules = schedule
+			.map((anime) => {
+				const { time, title, episode } = anime;
+				if (time && title && episode)
+					return `**${anime.time}**\t| ${anime.title} ***Ep${anime.episode}***`;
+			})
+			.join('\n');
 
 		const MsgToSend = new MessageEmbed()
 			.setColor('#ffff55')
@@ -31,14 +52,8 @@ module.exports = {
 				client.user.displayAvatarURL()
 			)
 			.setTitle(`:alarm_clock: Animes en Emision`)
-			.addField(
-				`${dias[day - 1]} ${new Date().getDate()}`,
-				schedule.map((anime) => {
-					const { time, title, episode } = anime;
-					if (time && title && episode)
-						return `**${anime.time}**\t| ${anime.title} ***Ep${anime.episode}***`;
-				})
-			);
+			.addField(`${field_date} ${field_day}/${field_month}`, animeSchedules);
+
 
 		return channel.send({ embeds: [MsgToSend] });
 	},
