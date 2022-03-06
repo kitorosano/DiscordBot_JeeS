72b3f220b79a39741d240e1ca206fa13205225bf
Kitorosano <rosanoesteban2@gmail.com>
2022-03-06 12:45:02 -0300

    Fixed animejs, empty embed msg

--- commands/anime.js
+++ commands/anime.js
@@ -1,35 +1,48 @@
-const {MessageEmbed} = require('discord.js');
-const _anime = require("jkanime");
+const { MessageEmbed } = require('discord.js');
+const _anime = require('jkanime');
 
-const dias = ["LUNES", "MARTES", "MIERCOLES", "JUEVES", "VIERNES", "SABADO", "DOMINGO"];
-
+const dias = [
+	'LUNES',
+	'MARTES',
+	'MIERCOLES',
+	'JUEVES',
+	'VIERNES',
+	'SABADO',
+	'DOMINGO',
+];
 
 module.exports = {
 	name: 'anime',
-  description: 'Va de anime',
-  // aliases: [''],
-  // usage: '[usuario]',
-  guildOnly: true,
-  async execute(msg, args, isMod) {
-    const {author, guild, channel, client} = msg;
+	description: 'Va de anime',
+	// aliases: [''],
+	// usage: '[usuario]',
+	guildOnly: true,
+	async execute(msg, args, isMod) {
+		const { author, guild, channel, client } = msg;
 
-		const day = (new Date().getHours() < 3 ? new Date().getDay() - 1 : new Date().getDay())
+		const day = new Date().getHours() < 3 ? new Date().getDay() - 1 : new Date().getDay();
+
+		const schedule = await _anime.schedule(day);
 
 		const MsgToSend = new MessageEmbed()
-											.setColor('#ffff55')
-											.setFooter('Provisto por animeschedule.net', client.user.displayAvatarURL())
-
-		if (!args.length) {
-			const schedule = await _anime.schedule(day)
-			MsgToSend.setTitle(`:alarm_clock: Animes en Emision`)
-							 .addField(`${dias[day -1]} ${new Date().getDate()}`, schedule.map(anime => {
-								 const {time, title, episode} = anime;
-								 if(time && title && episode) return `**${anime.time}**\t| ${anime.title} ***Ep${anime.episode}***`
-							 }))
-		};
+			.setColor('#ffff55')
+			.setFooter(
+				'Provisto por animeschedule.net',
+				client.user.displayAvatarURL()
+			)
+			.setTitle(`:alarm_clock: Animes en Emision`)
+			.addField(
+				`${dias[day - 1]} ${new Date().getDate()}`,
+				schedule.map((anime) => {
+					const { time, title, episode } = anime;
+					if (time && title && episode)
+						return `**${anime.time}**\t| ${anime.title} ***Ep${anime.episode}***`;
+				})
+			);
+		// };
 
 		// Buscar por dia
 
-		return channel.send({embeds: [MsgToSend]})
+		return channel.send({ embeds: [MsgToSend] });
 	},
 };
