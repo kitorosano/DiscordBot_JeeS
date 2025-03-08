f04a2d5afded335cfba838e8982a10f6594c8a2d
kitorosano <13912024+kitorosano@users.noreply.github.com>
2025-03-08 16:33:40 +0000

    feat: change keep-alive endpoint to /

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const { port: PORT } = require('./config.js');
+const express = require("express");
+const { port: PORT } = require("./config.js");
 
 const app = express();
 
-app.get('/keep-alive', (req, res) => {
-	res.send('Hello world from Replit')
+app.get("/", (req, res) => {
+	res.send("Hello world from Replit");
 });
 
 app.listen(PORT, () => console.log(`App listening at port: ${PORT}`));
 
-const botjs = require('./bot.js')
+const botjs = require("./bot.js");
