cac7bd41a55382940fc400d745a148e4c0db4cb9
Kitorosano <rosanoesteban2@gmail.com>
2025-03-08 11:16:33 -0300

    feat: :sparkles: agregado endpoint para keep-alive

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const { port: PORT } = require('./config.js');
 
 const app = express();
 
+app.get('/keep-alive', (req, res) => {
+	res.send('Hello world from Glitch')
+});
+
 app.listen(PORT, () => console.log(`App listening at port: ${PORT}`));
 
 const botjs = require('./bot.js')
