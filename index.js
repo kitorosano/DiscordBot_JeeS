a64959cd77f2980f4a1e70123fff3924d25462c0
kitorosano <13912024+kitorosano@users.noreply.github.com>
2025-03-11 03:29:39 +0000

    refactor: modificado el endpoint para ping 24/7

--- index.js
+++ index.js
@@ -3,21 +3,18 @@ const { port: PORT } = require("./config.js");
 
 const app = express();
 
-app.get("/", (req, res) => {
+// Route specifically for uptime monitoring services
+app.get("/ping", (req, res) => {
 	const datetime = new Date().toLocaleString("es-UY", {
 		timeZone: "America/Montevideo",
 		hour12: false,
 	});
-	console.log(`Received ping at ${datetime}`);
-	res.send("Hello world from Replit");
-});
-
-// Route specifically for uptime monitoring services
-app.get("/ping", (req, res) => {
-	console.log(`Health check ping received at ${new Date().toISOString()}`);
+	console.log(`Health check ping received at ${datetime}`);
 	res.status(200).send("Bot is running!");
 });
 
-app.listen(PORT, "0.0.0.0", () => console.log(`App listening at port: ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+	console.log(`App listening at port: ${PORT}`),
+);
 
 const botjs = require("./bot.js");
